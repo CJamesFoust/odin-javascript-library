61f956e214e5d9479528a2e90afd7e4f8986a6e6
CJamesFoust <82835548+CJamesFoust@users.noreply.github.com>
2025-08-21 00:45:23 -0400

    Completely change how everything works in the back-end. Implement save button.

--- helpers.js
+++ helpers.js
@@ -1,3 +1,20 @@
+export var library = [];
+
+export var currentBook = {};
+
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+const pagesInput = document.querySelector('#pages-input')
+const isReadInput = document.querySelector('#is-read');
+const colorInput = document.querySelector('#color-selector');
+const bookModal = document.querySelector("dialog");
+const radioRead = document.querySelector("#radio-read");
+const readShelf = document.querySelector('.read');
+const unreadShelf = document.querySelector('.unread');
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookRead = document.querySelector("#radio-read");
+
 export function HEXToL(hex) {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
@@ -19,9 +36,17 @@ export function HEXToL(hex) {
     return Math.round(((max + min) / 2) * 100);
 }
 
-export var library = [];
+export function getShelf(book) {
+    if(JSON.parse(book.isRead) === true) {
+        return readShelf;
+    } else if(JSON.parse(book.isRead) === false) {
+        return unreadShelf;
+    } else {
+        console.error('Something went wrong finding correct shelf.')
+    }
+}
 
-export function Book(title, author, numOfPages, isRead, color) {
+export function Book(title, author, numOfPages, isRead, color, id) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -31,7 +56,7 @@ export function Book(title, author, numOfPages, isRead, color) {
     this.numOfPages = numOfPages;
     this.isRead = isRead;
     this.color = color
-    this.id = 'i' + crypto.randomUUID()
+    this.id = id ? id : 'i' + crypto.randomUUID();
 }
 
 Book.prototype.info = function () {
@@ -42,11 +67,12 @@ export function addBookToLibrary(book) {
     library.push(book);
 }
 
-export function deleteBookFromLibrary(bookID, isRead) {
-    library.splice(library.findIndex(i => i.id === bookID), 1)
+export function addBookToLocalLibrary(book) {
+    library.push(book);
+    localStorage.setItem('Books', JSON.stringify(library));
 }
 
-export function addBookToShelf(book, shelf) {
+export function createBookElement(book) {
     let newBookDiv = document.createElement("div")
     newBookDiv.classList = "book";
     newBookDiv.textContent = book.title;
@@ -58,27 +84,21 @@ export function addBookToShelf(book, shelf) {
     if (book.color && book.color !== null && book.color !== "") {
         newBookDiv.style.backgroundColor = book.color;
         newBookDiv.setAttribute('data-color', book.color)
-
-        if (HEXToL(book.color) >= 50) {
-            newBookDiv.style.color = "#000000"
-        } else {
-            newBookDiv.style.color = "#FFFFFF"
-        }
+        HEXToL(book.color) >= 50 ? newBookDiv.style.color = "#000000" : newBookDiv.style.color = "#FFFFFF";
     }
 
     newBookDiv.addEventListener("click", (e) => {
         openBook(e.target);
     })
 
-    shelf.appendChild(newBookDiv);
+    return newBookDiv;
 }
 
-export function deleteBookFromShelf(bookID) {
-    let bookToDelete = document.querySelector(`#${bookID}`)
-    bookToDelete.remove();
+export function addBookToShelf(bookElement, shelf) {
+    shelf.appendChild(bookElement);
 }
 
-export function resetFieldValues(titleInput, authorInput, pagesInput, isReadInput, colorInput) {
+export function resetFieldValues() {
     titleInput.value = null;
     authorInput.value = null;
     pagesInput.value = null;
@@ -87,80 +107,89 @@ export function resetFieldValues(titleInput, authorInput, pagesInput, isReadInpu
 }
 
 export function booksFromLocal() {
-    let parsedReadBooks = [];
-    let parsedUnreadBooks = [];
-    let books = []
-    let readShelf = document.querySelector(".read");
-    let unreadShelf = document.querySelector(".unread");
-    
-    parsedReadBooks.push(JSON.parse(localStorage.getItem('ReadBooks')));
-    parsedUnreadBooks.push(JSON.parse(localStorage.getItem('UnreadBooks')));
+    // This seems complicated but isn't too bad. We're conditionally setting this variable based on what is in the local storage. First we check to see if there is even a record, if not (null), we set it as an empty array.
+    // If there is a record, we then check if the record is in the format of an array already, or if it's an object. If it's already in an Array format, we do Array.from, otherwise we create an array with the object record inside.
+    let booksInLibrary = localStorage.getItem('Books') === null ? [] : (Array.isArray(JSON.parse(localStorage.getItem('Books'))) ? Array.from(JSON.parse(localStorage.getItem('Books'))) : [JSON.parse(localStorage.getItem('Books'))]);
 
-    if((parsedReadBooks === null || parsedReadBooks.length === 0) && (parsedUnreadBooks === null || parsedUnreadBooks.length === 0)) {
-        return;
-    } else if((parsedReadBooks === null || parsedReadBooks.length === 0) && (parsedUnreadBooks !== null && parsedUnreadBooks !== "" && parsedUnreadBooks.length !== 0)) {
-        books.push(parsedUnreadBooks);
-    } else if ((parsedUnreadBooks === null || parsedUnreadBooks.length === 0) && (parsedReadBooks !== null && parsedReadBooks !== "" && parsedReadBooks.length !== 0)) {
-        books.push(parsedReadBooks);
-    } else {
-        books.push(parsedReadBooks.concat(parsedUnreadBooks));
+    if (booksInLibrary.length > 0) {
+        instantiateLibrary(booksInLibrary);
     }
-
-    books.map((book) => {
-        let bookObject = new Book(book.title, book.author, book.numOfPages, book.isRead, book.color)
-        addBookToLibrary(bookObject);
-        addBookToShelf(book, JSON.parse(book.isRead) ? readShelf : unreadShelf)
-    })
 }
 
-export function deleteBookFromLocal(bookID, isRead) {
-    let bookRead = JSON.parse(isRead) ? 'ReadBooks' : 'UnreadBooks';
-    let lStorage = [];
-    lStorage.push(JSON.parse(localStorage.getItem(bookRead)));
-
-    lStorage.splice(lStorage.findIndex(i => i.id === bookID), 1);
-    localStorage.setItem(bookRead, JSON.stringify(lStorage));
+export function deleteBookFromShelf(bookID) {
+    let bookToDelete = document.querySelector(`#${bookID}`)
+    bookToDelete.remove();
 }
 
-export function setLocalStorage(shelf, item) {
-    let lStorage = JSON.parse(localStorage.getItem(shelf));
-
-    if (lStorage !== null) {
-        localStorage.setItem(shelf, JSON.stringify(lStorage.concat(item)));
-    } else {
-        localStorage.setItem(shelf, JSON.stringify(item));
-    }
+export function deleteBookFromLibrary(bookID) {
+    library.splice(library.findIndex(i => i.id === bookID), 1)
+    localStorage.setItem('Books', JSON.stringify(library));
 }
 
 export function closeBook() {
     let bookModal = document.querySelector("dialog");
+    currentBook = {};
     bookModal.close()
+    bookModal.style.backgroundColor = "#F33139";
+    bookModal.style.boxShadow = `2px 6px 40px 0px #F33139`;
 }
 
 export function openBook(book) {
-    let bookModal = document.querySelector("dialog");
-    let bookTitle = document.querySelector("#title");
-    let bookAuthor = document.querySelector("#author");
-    let bookRead = document.querySelector("#radio-read")
-    bookTitle.textContent = book.getAttribute('title');
-    bookAuthor.textContent = book.getAttribute('author');
+    let indexOfBook = library.findIndex(i => i.id === book.id, 1);
+    currentBook = library[indexOfBook]
 
-    if (JSON.parse(book.getAttribute('isRead'))) {
-        bookRead.checked = true;
-    } else {
-        bookRead.checked = false;
-    }
-
-    bookModal.style.backgroundColor = book.getAttribute('data-color');
-    bookModal.style.boxShadow = `2px 6px 40px 0px ${book.getAttribute('data-color')}`;
-    bookModal.setAttribute('bookID', book.id);
+    bookTitle.textContent = currentBook.title;
+    bookAuthor.textContent = currentBook.author;
+    bookRead.checked = currentBook.isRead ? true : false;
+    bookModal.style.backgroundColor = currentBook.color;
+    bookModal.style.boxShadow = `2px 6px 40px 0px ${currentBook.color}`;
     bookModal.showModal();
 }
 
-export function deleteBook(book) {
-    let bookModal = document.querySelector("dialog");
-    deleteBookFromLibrary(book.id, book.getAttribute('isRead'));
-    deleteBookFromLocal(book.id, book.getAttribute('isRead'));
-    deleteBookFromShelf(bookModal.getAttribute('bookID'));
+export function deleteBook() {
+    deleteBookFromLibrary(currentBook.id);
+    deleteBookFromShelf(currentBook.id);
+    closeBook();
+}
+
+export function createBook() {
+    let book = new Book(titleInput.value, authorInput.value, pagesInput.value, isReadInput.value, colorInput.value)
+    addBookToLocalLibrary(book);
+    addBookToShelf(createBookElement(book), getShelf(book));
+    resetFieldValues();
+}
+
+export function saveBook() {
+    if((radioRead.checked === true && currentBook.isRead === true) || (radioRead.checked === false && currentBook.isRead === false) ) {
+        return;
+    } else {
+        updateLibraryBook();
+    }
+}
+
+export function updateLibraryBook() {
+    let indexOfBook = library.findIndex(i => i.id === currentBook.id, 1);
+    library[indexOfBook].isRead = !JSON.parse(library[indexOfBook].isRead);
+    refreshLibrary();
+}
+
+export function instantiateLibrary(booksInLibrary) {
+    booksInLibrary.map((book) => {
+        let bookObject = new Book(book.title, book.author, book.numOfPages, book.isRead, book.color, book.id);
+        addBookToLibrary(bookObject);
+        addBookToShelf(createBookElement(book), getShelf(book));
+    })
+}
+
+export function refreshLibrary() {
+    localStorage.setItem('Books', JSON.stringify(library));
+   
+    readShelf.replaceChildren();
+    unreadShelf.replaceChildren();
+    
+    library.map((book) => {
+        addBookToShelf(createBookElement(book), getShelf(book));
+    })
+
     closeBook();
 }
--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-import { Book, library, addBookToShelf, resetFieldValues, booksFromLocal, setLocalStorage, addBookToLibrary, closeBook, deleteBook } from "./helpers.js";
+import { booksFromLocal, closeBook, deleteBook, createBook, saveBook } from "./helpers.js";
 
 window.onload = booksFromLocal();
 
-var readBooks = [];
-var unreadBooks = [];
-
-var readBookshelf = document.querySelector(".read")
-var unreadBookshelf = document.querySelector(".unread")
-var addBookButton = document.querySelector("#add-book-btn")
-var closeBookButton = document.querySelector("#close-book")
-var deleteBookButton = document.querySelector("#delete-book")
-const titleInput = document.querySelector('#title-input');
-const authorInput = document.querySelector('#author-input');
-const pagesInput = document.querySelector('#pages-input')
-const isReadInput = document.querySelector('#is-read');
-const colorInput = document.querySelector('#color-selector');
+var addBookButton = document.querySelector("#add-book-btn");
+var closeBookButton = document.querySelector("#close-book");
+var deleteBookButton = document.querySelector("#delete-book");
+var saveBookButton = document.querySelector("#save-book");
 
 addBookButton.addEventListener("click", (e) => {
     e.preventDefault();
-    let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, isReadInput.value, colorInput.value)
-    addBookToLibrary(newBook);
-
-    if (isReadInput.value == 'true') {
-        readBooks.push(newBook);
-        addBookToShelf(newBook, readBookshelf);
-        setLocalStorage('ReadBooks', newBook);
-    } else {
-        unreadBooks.push(newBook)
-        addBookToShelf(newBook, unreadBookshelf);
-        setLocalStorage('UnreadBooks', newBook);
-    }
-
-    resetFieldValues(titleInput, authorInput, pagesInput, isReadInput, colorInput);
+    createBook();
 });
 
 closeBookButton.addEventListener("click", (e) => {
@@ -42,4 +20,9 @@ closeBookButton.addEventListener("click", (e) => {
 deleteBookButton.addEventListener("click", (e) => {
     e.preventDefault();
     deleteBook(e.target);
+})
+
+saveBookButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    saveBook();
 })
